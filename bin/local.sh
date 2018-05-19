793d0cfa43aba7d61075fde7104a733d7deb65b3
Paul Wright <pwright@redhat.com>
2018-05-19 12:31:53 +0100

    remote partials

--- bin/local.sh
+++ bin/local.sh
@@ -1,3 +1,8 @@
+# download requirements
+
+curl https://raw.githubusercontent.com/finp/trello/master/modules/test/pages/test.adoc > modules/test/pages/rantora-test.adoc
+
+
 # create html
 
 antora local-site.yml
